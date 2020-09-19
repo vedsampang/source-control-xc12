4826869bac88bad700b98bddb50dacde408e2adf
Ved Rai <vedsampang@gmail.com>
2020-09-19 19:03:19 -0400

    Added a print message

--- Source Control/ViewController.swift
+++ Source Control/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: NSViewController {
      - returns: The sum of num1 and num2.
      */
     func addNumbers(num1:Int, num2:Int) -> Int {
+        print("Thing 1")
         return num1 + num2
     }
     
